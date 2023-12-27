258e9ce80396b26978c404464c3849406a4e84bb
Anurag <coleridgeprotube@gmail.com>
2023-12-27 14:29:13 +0530

    pushing page

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+app.listen(3000, ()=>{console.log("server running")})
